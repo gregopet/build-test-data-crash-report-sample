03ab54413a03bc8de9f114cce4a72eccac4609bd
Gregor Petrin <gregap@gmail.com>
2014-07-11 06:12:36 +0200

    Made the spec inherit a base class

--- test/unit/btd/test/AuthorSpec.groovy
+++ test/unit/btd/test/AuthorSpec.groovy
@@ -1,17 +1,9 @@
 package btd.test
 
-import grails.test.mixin.TestFor
-import grails.buildtestdata.mixin.Build
-import spock.lang.Specification
-import grails.test.mixin.hibernate.HibernateTestMixin
-import grails.test.mixin.gorm.Domain
 /**
 * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
 */
-@Domain(Author)
-@Build(Author)
-@TestMixin(HibernateTestMixin)
-class AuthorSpec extends Specification {
+class AuthorSpec extends SpecBase {
 
 	def setup() {
 	}
---
+++ test/unit/btd/test/SpecBase.groovy
@@ -0,0 +1,15 @@
+package btd.test
+
+import grails.test.mixin.TestFor
+import grails.buildtestdata.mixin.Build
+import spock.lang.Specification
+import grails.test.mixin.hibernate.HibernateTestMixin
+import grails.test.mixin.gorm.Domain
+/**
+* See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+*/
+@Domain(Author)
+@Build(Author)
+@TestMixin(HibernateTestMixin)
+class SpecBase extends Specification {
+}
